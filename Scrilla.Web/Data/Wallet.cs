af74deff727d27deb99e55a449eb0698da4b383a
Tyson Elford <tyelford@gmail.com>
2021-02-21 14:48:13 +1000

    Started a very basic front end

---
+++ Scrilla.Web/Data/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scrilla.Web.Data
+{
+    public class Wallet
+    {
+        public string ExchangeName { get; set; }
+        public string CoinName { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ Scrilla.Web/Data/WalletService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scrilla.Web.Data
+{
+    public class WalletService
+    {
+
+        public async Task<List<Wallet>> GetAllWalletsAsync()
+        {
+            //Do something to get all wallets from all exchanges here
+            var wallets = new List<Wallet>();
+
+            wallets.Add(new Wallet
+            {
+                ExchangeName = "Newton",
+                CoinName = "BTC",
+                Amount = 0.00341
+            });
+
+            wallets.Add(new Wallet
+            {
+                ExchangeName = "Binance",
+                CoinName = "BTC",
+                Amount = 0.00308
+            });
+
+            await Task.Delay(0);
+
+            return wallets;
+
+        }
+
+    }
+}
