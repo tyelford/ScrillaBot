aa26786db4ffcafaba5541661ec9c5bde22eae95
Tyson Elford <tyelford@gmail.com>
2021-03-04 14:51:16 +1000

    Added some rough charts to main pages

--- Scrilla.Web/Startup.cs
+++ Scrilla.Web/Startup.cs
@@ -38,6 +38,7 @@ namespace Scrilla.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
